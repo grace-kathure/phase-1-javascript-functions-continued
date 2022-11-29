b546c7850b89c96cb67b89a15420382e391c2731
grace-kathure <gracegikundak@gmail.com>
2022-11-29 16:31:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return (`This Saturday, I want to ${activity}!`)
+}
+saturdayFun()
+saturdayFun("practice coding")
+
+const mondayWork = function (activity = "go to the office"){
+    return (`This Monday, I will ${activity}.`)
+}
+mondayWork()
+mondayWork("practice coding")
+
+function wrapAdjective(string = "*"){
+    return function(name = "special"){
+        return `You are ${string}${name}${string}!`
+    }   
+
+}
+wrapAdjective("%")("a committed Christian")
+
+
